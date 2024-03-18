f79f8801d2a5a9df45ebdb41b2aa0bf9ff909101
Revo1999 <98609917+Revo1999@users.noreply.github.com>
2024-03-18 14:51:52 +0100

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -11,15 +11,15 @@ printf "${green}Dialog installed \n"
 
 
 printf "${yellow}Loading Git Credentials \n"
-bash VictorRasmussen#3454/setup_script/git.sh
+bash YOUR_USERNAME/setup_script/git.sh
 printf "${green}Git Credentials loaded \n"
 
 
 printf "${yellow}\nfile explorer copying into bin\n"
-sudo cp VictorRasmussen#3454/setup_script/filex.sh /home/ucloud/.local/bin
+sudo cp YOUR_USERNAME/setup_script/filex.sh /home/ucloud/.local/bin
 sudo ln -s /home/ucloud/.local/bin/filex.sh /home/ucloud/.local/bin/filex
 printf "${green}\nfile explorer created\n"
 
 printf "${yellow}\nsetting file explorer style\n"
-sudo cp VictorRasmussen#3454/setup_script/.dialogrc /home/ucloud
+sudo cp YOUR_USERNAME/setup_script/.dialogrc /home/ucloud
 printf "${green}\nfile explorer created\n"
